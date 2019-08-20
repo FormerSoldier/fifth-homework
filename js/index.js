21745904666ea14717d4dfb969ba39b9aeaef867
ZHANGGA5 <enju648454858>
2019-08-20 20:19:13 +0800

    feat: feat the grid support edit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 Ext.onReady(function(){
+
     var operateRecord = {};
     let window = new Ext.Window({
         id:'window',
@@ -57,9 +58,9 @@ Ext.onReady(function(){
                     });
 
                     if(operateRecord.selectRowIndex == undefined || operateRecord.selectRowIndex == null)
-                        store.insert(0,record);
+                        groupingStore.insert(0,record);
                     else
-                        store.insert(operateRecord.selectRowIndex,record);
+                        groupingStore.insert(operateRecord.selectRowIndex,record);
 
                     for(let i = 0; i < inputValue.length; i++)
                         inputValue[i].setValue('');
@@ -81,18 +82,6 @@ Ext.onReady(function(){
         }
     });
 
-    let menu = new Ext.menu.Menu({
-        items:[
-            {
-                text:'add',
-                handler:function(){
-                    window.show();
-                }
-            }
-        ]
-    })
-
-
     let data = [
         ['刘一','A','male',new Date(),'1'],
         ['陈二','A','female',new Date(),'2'],
@@ -102,7 +91,15 @@ Ext.onReady(function(){
     ]
 
     let cm = new Ext.grid.ColumnModel([
-        {header:'name',dataIndex:'name'},
+        {
+            header:'name',
+            dataIndex:'name',
+            editor: new Ext.grid.GridEditor(
+                new Ext.form.TextField({
+                    allowBlank: false
+                })
+            )
+        },
         {header:'class',dataIndex:'className'},
         {header:'sex',dataIndex:'sex'},
         {header:'birthday',dataIndex:'birthday',renderer: Ext.util.Format.dateRenderer('Y/m/d')},
@@ -124,18 +121,62 @@ Ext.onReady(function(){
     
     groupingStore.load();
 
-    
+    let contextMenu = new Ext.menu.Menu({
+        items:[
+            {
+                text:'Up',
+                handler:function(){
 
-    let grid = new Ext.grid.GridPanel({
+                }
+            },
+            {
+                text:'Down',
+                handler:function(){
+                    
+                }
+            },
+            {
+                text:'First',
+                handler:function(){
+                    
+                }
+            },
+            {
+                text:'Last',
+                handler:function(){
+                    
+                }
+            }
+        ]
+    });
+
+    let grid = new Ext.grid.EditorGridPanel({
         renderTo:'test',
         height:500,
         store:groupingStore,
         cm:cm,
-        tbar:[{text:'总菜单',menu:menu}],
+        contextMenu:contextMenu,
+        tbar:[{
+            text:'add',
+            handler:function(){
+                window.show();
+            }
+        },'-',{
+            text:'move',
+            handler:function(){
+                console.log(operateRecord);
+            }
+        }],
         view: new Ext.grid.GroupingView(),
         listeners:{
             rowclick: function(grid, rowIndex){
                 operateRecord.selectRowIndex = rowIndex;
+            },
+            rowcontextmenu:function(grid, rowIndex, event){
+                event.preventDefault();
+                grid.getSelectionModel().selectRow(rowIndex);
+                contextMenu.showAt(event.getXY());
+                console.log(grid.getSelectionModel().getSelected())
             }
         }
     });
